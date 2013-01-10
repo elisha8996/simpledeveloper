7027d3dd75cf800ea9424332a938af4e08b8331e
Eenvincible <elisha.java@yahoo.com>
2013-01-09 20:54:08 -0800

    Create twittersearchapi.py
    Two functions that work together to search and return data from Twitter Servers

---
+++ twittersearchapi.py
@@ -0,0 +1,38 @@
+#Simple Developer Blog
+#Python code to make simple Twitter searches using
+#user entered arguments
+#2013 - Mozilla P2PU Challenge - Programming with Twitter API
+
+import json
+import urllib2
+
+def search_twitter(query):
+    #take care of any possible exceptions from bad api calls
+    url = 'http://search.twitter.com/search.json?q='+query         #add the query to the url
+    try:
+        tweets = urllib2.urlopen(url)                              #make the call to twitter
+        parsed_data = json.load(tweets)                            #load using JSON
+        return print_tweets(parsed_data)
+    except urllib2.URLError, e:                                    #catch any errors that might occur
+        print e.reason
+        raise
+
+#define our print_tweets() function
+def print_tweets(data):
+    i = 0
+    while i < len(data['results']):                                           #use a while loop and a counter
+        print '@%s tweeted: %s on %s' % (data['results'][i]['from_user'],     #access ['from_user'] data
+                                         data['results'][i]['text'],          #access ['text'] data
+                                         data['results'][i]['created_at'])    #access ['created_at'] data
+        i += 1                                                                #Increment the counter here
+
+#Call the function and see the results
+if __name__ == '__main__':
+    while True:
+        query = raw_input("Enter a search term:")                  #prompt the user for a search term
+        if not query:                                              #if no term, break, else
+            break
+        search_twitter(query)                                      #call the function and get the result
+
+    
+    
